cc9592500599324bde95c30c88096c69e97ee09d
robbar-dev <ro.barbir@gmail.com>
2025-03-03 14:59:56 +0100

    experiment overfitting with weight_decay

--- training/trainer.py
+++ training/trainer.py
@@ -84,7 +84,7 @@ class TripletTrainerBase(nn.Module):
 
         # 4) Optimizer
         params = list(self.base_cnn.parameters()) + list(self.mil_agg.parameters())
-        self.optimizer = optim.Adam(params, lr=self.lr)
+        self.optimizer = optim.Adam(params, lr=self.lr, weight_decay=1e-5)
 
         # Tracking
         self.epoch_losses = []          # Gesamt-Loss pro Epoche
